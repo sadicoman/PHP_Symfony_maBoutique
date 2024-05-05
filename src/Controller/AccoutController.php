e1ed6d6fa27ca0d2a4ddcfd4c4261a9283ac56f6
Francois Szczepkowski <francois.szcz@hotmail.fr>
2024-05-05 16:10:49 +0200

    ADD: Modification du mot de passe utilisateur

--- src/Controller/AccoutController.php
+++ src/Controller/AccoutController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Form\PasswordUserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AccoutController extends AbstractController
@@ -12,5 +17,25 @@ class AccoutController extends AbstractController
     public function index(): Response
     {
         return $this->render('accout/index.html.twig');
+    }
+
+    #[Route('/compte/modifier-mot-de-passe', name: 'app_account-modify_pwd')]
+    public function password(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(PasswordUserType::class, $user, [
+            'passwordHasher' => $passwordHasher
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+        }
+
+        return $this->render('accout/password.html.twig', [
+            'modifyPwd' => $form->createView(),
+        ]);
     }
 }
---
+++ src/Form/PasswordUserType.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
+class PasswordUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('actualPassword', PasswordType::class, [
+                'label' => 'Votre mot de passe actuel',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre mot de passe actuel'
+                ],
+                'mapped' => false,
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'constraints' => [
+                    new Length([
+                        'min' => 4,
+                        'max' => 40,
+                    ])
+                ],
+                'first_options'  => [
+                    'label' => 'Votre nouveau mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Indiquez votre nouveau mot de passe'
+                    ],
+                    'hash_property_path' => 'password'
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer votre nouveau mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Confirmer votre nouveau mot de passe'
+                    ],
+                ],
+                'mapped' => false,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Mettre à jour mon mot de passe',
+                'attr' => [
+                    'class' => 'btn-success'
+                ],
+            ])
+            ->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) {
+                $form = $event->getForm();
+
+                $user = $form->getConfig()->getOptions()['data'];
+
+                $passwordHasher = $form->getConfig()->getOptions()['passwordHasher'];
+
+                $actualPwd = $form->get('actualPassword')->getData();
+
+                $isValid = $passwordHasher->isPasswordValid(
+                    $user,
+                    $actualPwd
+                );
+
+                // $actualPwdDatabase = $user->getPassword();
+
+                if (!$isValid) {
+                    $form->get('actualPassword')->addError(new FormError("Votre mot de passe actuel n'est pas conforme. Veuillez vérifier votre saisie."));
+                }
+            });
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'passwordHasher' => null
+        ]);
+    }
+}
